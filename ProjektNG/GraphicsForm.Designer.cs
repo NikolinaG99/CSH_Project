ca01412ee90170b06c371521ceb4095891ba5603
Nikolina Govorušić <nina.govorusic@gmail.com>
2023-01-09 10:51:38 +0100

    Add project files.

---
+++ ProjektNG/GraphicsForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace ProjektNG
+{
+    partial class GraphicsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawing = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pointBtn = new System.Windows.Forms.Button();
+            this.lineBtn = new System.Windows.Forms.Button();
+            this.circleBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.drawing)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // drawing
+            // 
+            this.drawing.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.drawing.BackColor = System.Drawing.SystemColors.Window;
+            this.drawing.Location = new System.Drawing.Point(0, 2);
+            this.drawing.Name = "drawing";
+            this.drawing.Size = new System.Drawing.Size(861, 488);
+            this.drawing.TabIndex = 0;
+            this.drawing.TabStop = false;
+            this.drawing.Click += new System.EventHandler(this.drawing_Click);
+            this.drawing.Paint += new System.Windows.Forms.PaintEventHandler(this.drawing_Paint);
+            this.drawing.MouseDown += new System.Windows.Forms.MouseEventHandler(this.drawing_MouseDown);
+            this.drawing.MouseMove += new System.Windows.Forms.MouseEventHandler(this.drawing_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 511);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pointBtn
+            // 
+            this.pointBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pointBtn.Location = new System.Drawing.Point(882, 12);
+            this.pointBtn.Name = "pointBtn";
+            this.pointBtn.Size = new System.Drawing.Size(132, 42);
+            this.pointBtn.TabIndex = 3;
+            this.pointBtn.Text = "Point";
+            this.pointBtn.UseVisualStyleBackColor = true;
+            this.pointBtn.Click += new System.EventHandler(this.pointBtn_Click);
+            // 
+            // lineBtn
+            // 
+            this.lineBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lineBtn.Location = new System.Drawing.Point(882, 71);
+            this.lineBtn.Name = "lineBtn";
+            this.lineBtn.Size = new System.Drawing.Size(132, 42);
+            this.lineBtn.TabIndex = 4;
+            this.lineBtn.Text = "Line";
+            this.lineBtn.UseVisualStyleBackColor = true;
+            this.lineBtn.Click += new System.EventHandler(this.lineBtn_Click);
+            // 
+            // circleBtn
+            // 
+            this.circleBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.circleBtn.Location = new System.Drawing.Point(882, 135);
+            this.circleBtn.Name = "circleBtn";
+            this.circleBtn.Size = new System.Drawing.Size(132, 42);
+            this.circleBtn.TabIndex = 5;
+            this.circleBtn.Text = "Circle";
+            this.circleBtn.UseVisualStyleBackColor = true;
+            this.circleBtn.Click += new System.EventHandler(this.circleBtn_Click);
+            // 
+            // GraphicsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1035, 587);
+            this.Controls.Add(this.circleBtn);
+            this.Controls.Add(this.lineBtn);
+            this.Controls.Add(this.pointBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.drawing);
+            this.Name = "GraphicsForm";
+            this.Text = "GraphicsForm";
+            this.Load += new System.EventHandler(this.GraphicsForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.drawing)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox drawing;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button pointBtn;
+        private System.Windows.Forms.Button lineBtn;
+        private System.Windows.Forms.Button circleBtn;
+    }
+}
+
---
+++ ProjektNG/GraphicsForm.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjektNG
+{
+    public partial class GraphicsForm : Form
+    {
+        public GraphicsForm()
+        {
+            InitializeComponent();
+        }
+        private List<Entities.Point> points = new List<Entities.Point>();
+        private List<Entities.Line> lines = new List<Entities.Line>();
+        private List<Entities.Circle>circles= new List<Entities.Circle>();
+        private Vector3 currentPosition;
+        private Vector3 firstPoint;
+        private int DrawIndex = -1;
+        private bool active_drawing = false;
+        private int ClickNum = 1;
+
+        private void GraphicsForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void drawing_MouseMove(object sender, MouseEventArgs e)
+        {
+            currentPosition =PointToCartesian(e.Location);
+            label1.Text=string.Format("{0}, {1}",e.Location.X,e.Location.Y); //koordinantni sustav
+            drawing.Refresh();
+           // label2.Text = string.Format("{0,0:F3}, {1,0:F3}", currentPosition.X, currentPosition.Y);
+        }
+
+        //dobili smo screen dpi
+        private float DPI
+        {
+            get
+            {
+                using (var g = CreateGraphics())
+                    return g.DpiX;
+            }
+        }
+        //convert system point to word1 point
+        private Vector3 PointToCartesian(Point point)
+        {
+            return new Vector3(Pixel_to_Mn(point.X), Pixel_to_Mn(drawing.Height-point.Y));
+        }
+        //pretvorba piksela u milimetre
+        private float Pixel_to_Mn(float pixel)
+        {
+            return pixel * 25.4f / DPI;
+        }
+
+        private void drawing_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button== MouseButtons.Left)
+            {
+                if (active_drawing)
+                {
+                    switch(DrawIndex)
+                    {
+                        case 0: //point
+                            points.Add(new Entities.Point(currentPosition));
+                                break;
+                       case 1:// line
+                            switch (ClickNum)
+                            {
+                                case 1:
+                                    firstPoint = currentPosition;
+                                    points.Add(new Entities.Point(currentPosition));
+                                    ClickNum++;
+                                    break;
+                                case 2:
+                                    lines.Add(new Entities.Line(firstPoint, currentPosition));
+                                    points.Add(new Entities.Point(currentPosition));
+                                    firstPoint =currentPosition;
+                                    break;
+                            }
+                            break;
+                       case 2: // circle
+                            switch (ClickNum)
+                            {
+                                case 1:
+                                    firstPoint = currentPosition;
+                                    ClickNum++;
+                                    break;
+                                case 2:
+                                    double r=firstPoint.DistanceFrom(currentPosition);
+                                    circles.Add(new Entities.Circle(firstPoint,r));
+                                    ClickNum = 1;
+                                    break;  
+                            }
+                            break;
+                    }
+                    drawing.Refresh();
+                }
+            }
+        }
+
+
+        private void drawing_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SetParameters(Pixel_to_Mn(drawing.Height));
+            Pen pen=new Pen(Color.Pink,0.1f);
+            Pen extpen = new Pen(Color.Blue, 0.1f);
+            //Draw all points
+            if (points.Count > 0)
+            {
+                foreach(Entities.Point p in points)
+                {
+                    e.Graphics.DrawPoint(new Pen(Color.Red, 0.1f), p);
+                }
+            }
+            // Draw all lines 
+            if (lines.Count > 0)
+            {
+                foreach(Entities.Line l in lines)
+                {
+                    e.Graphics.DrawLine(pen, l);
+                }
+            }
+            // Draw all circle
+            if (circles.Count > 0)
+            {
+                foreach(Entities.Circle c in circles)
+                {
+                    e.Graphics.DrawCircle(pen, c);
+                }
+            }
+            // Draw line extended
+            switch (DrawIndex)
+            {
+                case 1:
+                    if(ClickNum == 2)
+                    {
+                        Entities.Line line = new Entities.Line(firstPoint, currentPosition);
+                        e.Graphics.DrawLine(extpen, line);
+                    }
+                  
+                    break;
+                case 2:
+                    if (ClickNum == 2)
+                    {
+                        Entities.Line line = new Entities.Line(firstPoint, currentPosition);
+                        e.Graphics.DrawLine(extpen, line);
+                        double r=firstPoint.DistanceFrom(currentPosition);
+                        Entities.Circle circle = new Entities.Circle(firstPoint,r);
+                        e.Graphics.DrawCircle(extpen, circle); 
+                    }
+
+                    break;
+            }
+        }
+
+        private void drawing_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pointBtn_Click(object sender, EventArgs e)
+        {
+            DrawIndex = 0;
+            active_drawing = true;
+            drawing.Cursor = Cursors.Cross;
+        }
+
+        private void lineBtn_Click(object sender, EventArgs e)
+        {
+            DrawIndex = 1;
+            active_drawing = true;
+            drawing.Cursor = Cursors.Cross;
+        }
+
+        private void circleBtn_Click(object sender, EventArgs e)
+        {
+            DrawIndex = 2;
+            active_drawing = true;
+            drawing.Cursor = Cursors.Cross;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
